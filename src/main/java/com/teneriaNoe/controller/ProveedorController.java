f5681860dcc64df87687d58aef01a9ab86c714ef
bfranco <bfranco@ISBMPC-OC003236.isbm.gob.sv>
2017-03-28 00:35:00 -0600

    Se agrego pantalla de proveedores, ya funciona solomente mejorar el diseño de la pantalla.

--- src/main/java/com/teneriaNoe/controller/ProveedorController.java
+++ src/main/java/com/teneriaNoe/controller/ProveedorController.java
@@ -1,11 +1,9 @@
-
 package com.teneriaNoe.controller;
-
-
 
 import com.teneriaNoe.ejb.ProveedorFacadeLocal;
 import com.teneriaNoe.model.Proveedor;
 import java.io.Serializable;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
@@ -15,27 +13,48 @@ import javax.inject.Named;
  *
  * @author bfranco
  */
-
 @Named
 @ViewScoped
 public class ProveedorController implements Serializable {
- 
+
     @EJB
-   private ProveedorFacadeLocal proveedorEJB; 
+    private ProveedorFacadeLocal proveedorEJB;
     private Proveedor proveedor;
+    private List<Proveedor> proveedores;
     
     @PostConstruct
-    public void init(){
-    proveedor=new Proveedor();
+    public void init() {
+        proveedor = new Proveedor();
+        proveedores=proveedorEJB.findAll();
     }
-    
-    public void registrar(){
+
+    public void registrar() {
         try {
-           proveedorEJB.create(proveedor);
+            proveedorEJB.create(proveedor);
         } catch (Exception e) {
             e.printStackTrace();
         }
-    
+
+    }
+    //Getter y Setters
+
+    public Proveedor getProveedor() {
+        return proveedor;
     }
 
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    
+    
+    public List<Proveedor> getProveedores() {
+        return proveedores;
+    }
+
+    public void setProveedores(List<Proveedor> proveedores) {
+        this.proveedores = proveedores;
+    }
+    
+    
 }
